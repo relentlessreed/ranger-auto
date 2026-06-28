d6539ecaaaa5a981b3c325ce7881aa6e364f0a0e
relentlessreed <relentlessreed@gmail.com>
2026-06-28 01:55:21 -0500

    Refine header stripe SVG

--- components/public/header-stripe.tsx
+++ components/public/header-stripe.tsx
@@ -6,15 +6,9 @@ export function HeaderStripe() {
       viewBox="0 0 1600 18"
       preserveAspectRatio="none"
     >
-      <rect width="1600" height="18" fill="#ffffff" />
-      <path
-        d="M0 0H1600V5H390L368 18H0V0Z"
-        fill="#f5c319"
-      />
-      <path
-        d="M0 7H1600V11H340L360 7H0Z"
-        fill="#0f4c97"
-      />
+      <rect width="1600" height="18" fill="#f5c319" />
+      <path d="M0 3H355L380 8H1600V11H0V3Z" fill="#0f4c97" />
+      <path d="M0 0H420L392 3H0V0Z" fill="#ffffff" opacity="0.96" />
     </svg>
   );
 }
