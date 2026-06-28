1d912592811f3efc4ff129eb8c6e5355fa4cf6de
relentlessreed <relentlessreed@gmail.com>
2026-06-28 02:00:49 -0500

    Refine header stripe symmetry

--- components/public/header-stripe.tsx
+++ components/public/header-stripe.tsx
@@ -6,8 +6,8 @@ export function HeaderStripe() {
       viewBox="0 0 1600 18"
       preserveAspectRatio="none"
     >
-      <rect x="0" y="7" width="1600" height="4" fill="#174f96" />
-      <path d="M0 14H365L402 3H540V7H428L391 18H0V14Z" fill="#f2be1a" />
+      <rect x="0" y="6" width="1600" height="4" fill="#174f96" />
+      <path d="M0 13H368L397 0H1600V3H424L395 16H0V13Z" fill="#f2be1a" />
     </svg>
   );
 }
